ce35433b62963f60791d2858f12fc687210f4be1
bradshoow <mat.simonnet@gmail.com>
2014-02-11 23:36:38 +0100

    Wind and JSON are born

--- WindBuoy/src/orion/ms/windbuoy/MainActivity.java
+++ WindBuoy/src/orion/ms/windbuoy/MainActivity.java
@@ -6,7 +6,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,44 +18,51 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	TextView text = null; 
-	String str = "", velocity = ""; 
 	
-
+	private TextView directionTextView;
+	public TextView velocityTextView;
+	
+	Wind wind = new Wind();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+			
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		text = (TextView) findViewById(R.id.velocity);
-				
-		new Thread(new Runnable() {
-			
+		
+		directionTextView = (TextView) findViewById(R.id.direction);
+		velocityTextView = (TextView) findViewById(R.id.velocity);
+		
+		new Thread(new Runnable() {		
 			@Override
 			public void run() {
-				for(int i=0;i<10;i++){
+				for(int i=0;i<100;i++){
+					String data = request("http://pubs.diabox.com/dataUpdate.php?dbx_id=16&dataNameList[]=st-mathieu_wind_rt");
 					try {
-						Thread.sleep(5000);
-					} catch (InterruptedException e) {
-						Log.e("sleep", "PB");
-					}
-					str = request("http://pubs.diabox.com/dataUpdate.php?dbx_id=16&dataNameList[]=st-mathieu_wind_rt") + "" + i;
-					//velocity = str.split(""force":.*")
+						getWind(data, wind);
+					} catch (JSONException e1) {}
 					
-					text.post(new Runnable() {
-						
+					directionTextView.post(new Runnable() {
 						@Override
 						public void run() {
-							text.setText(str);
-							
+							directionTextView.setText(wind.direction);
+							if ( Float.parseFloat(wind.velocity) > 20) {
+								velocityTextView.setBackgroundColor(Color.RED);
+								velocityTextView.setScaleX(1.2f);
+							}
+							if ( Float.parseFloat(wind.velocity) < 20){
+								velocityTextView.setBackgroundColor(Color.BLUE);
+							}
+							velocityTextView.setText(wind.velocity);
+							velocityTextView.setScaleX(1.0f);
 						}
 					});
-						
-				}	
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {}
+				}
 			}
 		}).start();
-		
-
-		
 	}
 
 	@Override
@@ -78,5 +89,41 @@ public class MainActivity extends Activity {
 		return answer;
 	}
 
+	public void getWind(String data, Wind wind) throws JSONException{
+				
+		//-------> jObj
+		//{
+		//"st-mathieu_wind_rt":
+				
+				//-------> windObject
+				//{
+				//"date":1392148699, 
+				//"dir":270,
+				//"force":18.248
+				//}
+		
+		//}
+				
+		JSONObject jObj = new JSONObject(data);
+		Log.i("JSONObject jObj",jObj.toString());
+		
+		JSONObject windObject = getObject("st-mathieu_wind_rt", jObj);
+		Log.i("JSONObject windObject",windObject.toString());
+		
+		wind.setDirection(getString("dir", windObject));
+		Log.i("String Wind",wind.direction);
+		
+		wind.setVelocity(getString("force", windObject));
+		Log.i("String Wind",wind.velocity);
+	}
+	
+	private static JSONObject getObject(String tagName, JSONObject jObj)  throws JSONException {
+		JSONObject subObj = jObj.getJSONObject(tagName);
+		return subObj;
+	}
+	
+	private static String getString(String tagName, JSONObject jObj) throws JSONException {
+		return jObj.getString(tagName);
+	}
 
 }
