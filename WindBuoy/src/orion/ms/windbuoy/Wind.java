c036527e97ff1be72ec7d1931fb77cfbe5b85547
bradshoow <mat.simonnet@gmail.com>
2014-02-11 23:36:38 +0100

    Wind and JSON are born

---
+++ WindBuoy/src/orion/ms/windbuoy/Wind.java
@@ -0,0 +1,21 @@
+package orion.ms.windbuoy;
+
+public class Wind {
+
+	String direction = "";
+	String velocity = "";
+	
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getVelocity() {
+		return velocity;
+	}
+	public void setVelocity(String velocity) {
+		this.velocity = velocity;
+	}
+		
+}
