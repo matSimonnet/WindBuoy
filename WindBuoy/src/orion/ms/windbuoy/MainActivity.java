4f7bd351b105c990ca5593dea9106c257fd6271b
bradshoow <mat.simonnet@gmail.com>
2014-04-28 22:13:38 +0200

    test

--- WindBuoy/src/orion/ms/windbuoy/MainActivity.java
+++ WindBuoy/src/orion/ms/windbuoy/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-			
+			//test
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 			
